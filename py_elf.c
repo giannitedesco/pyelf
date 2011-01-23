a2e3ebd9c2942c67dcb43d6a638c6d3647a56848
Gianni Tedesco <gianni@scaramanga.co.uk>
2011-01-23 06:24:04 +0000

    Implement kind, bits, ident and ehdr methods for elf files

--- py_elf.c
+++ py_elf.c
@@ -5,22 +5,80 @@
 */
 
 #include <Python.h>
+#include <elf.h>
+#include <gelf.h>
 #include <libelf.h>
 
+/* Exception hierarchy */
 static PyObject *pyelf_err_base;
-static PyObject *pyelf_err_fmt;
+static PyObject 	*pyelf_err_fmt;
 
+/* Types */
 struct pyelf_elf {
 	PyObject_HEAD;
 	PyObject *file;
 	Elf *elf;
 };
 
+struct pyelf_ehdr {
+	PyObject_HEAD;
+	GElf_Ehdr ehdr;
+};
+
+
+/* Generic functions */
 static void pyelf_error(void)
 {
 	PyErr_SetString(PyExc_SystemError, elf_errmsg(elf_errno()));
 }
 
+static PyObject *pyelf_bytearray(const char *ptr, size_t sz)
+{
+	return PyByteArray_FromStringAndSize(ptr, sz);
+}
+
+
+/* Elf Ehdrs */
+static PyMethodDef pyelf_ehdr_methods[] = {
+	{NULL, }
+};
+
+static PyGetSetDef pyelf_ehdr_attribs[] = {
+	{NULL, }
+};
+
+static void pyelf_ehdr_dealloc(struct pyelf_ehdr *self)
+{
+	self->ob_type->tp_free((PyObject*)self);
+	return;
+}
+
+static PyTypeObject pyelf_ehdr_pytype = {
+	PyObject_HEAD_INIT(NULL)
+	.tp_name = "elf.ehdr",
+	.tp_basicsize = sizeof(struct pyelf_ehdr),
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_new = PyType_GenericNew,
+	.tp_dealloc = (destructor)pyelf_ehdr_dealloc,
+	.tp_methods = pyelf_ehdr_methods,
+	.tp_getset = pyelf_ehdr_attribs,
+	.tp_doc = "ELF header",
+};
+
+static PyObject *pyelf_ehdr_New(GElf_Ehdr *gehdr)
+{
+	struct pyelf_ehdr *ehdr;
+
+	ehdr = (struct pyelf_ehdr *)
+		pyelf_ehdr_pytype.tp_alloc(&pyelf_ehdr_pytype, 0);
+	if ( ehdr == NULL )
+		return NULL;
+	
+	memcpy(&ehdr->ehdr, gehdr, sizeof(ehdr->ehdr));
+	return (PyObject *)ehdr;
+}
+
+/* ELF files */
 static int pyelf_elf_init(struct pyelf_elf *self, PyObject *args, PyObject *kwds)
 {
 	PyObject *file;
@@ -68,7 +126,65 @@ static void pyelf_elf_dealloc(struct pyelf_elf *self)
 	self->ob_type->tp_free((PyObject*)self);
 }
 
+static PyObject *pyelf_elf_kind_get(struct pyelf_elf *self)
+{
+	return PyInt_FromLong(elf_kind(self->elf));
+}
+
+static PyObject *pyelf_elf_bits_get(struct pyelf_elf *self)
+{
+	int ret;
+
+	switch(gelf_getclass(self->elf)) {
+	case ELFCLASS32:
+		ret = 32;
+		break;
+	case ELFCLASS64:
+		ret = 64;
+		break;
+	default:
+		PyErr_SetString(pyelf_err_fmt, "Undetermined ELF format");
+		return NULL;
+	}
+
+	return PyInt_FromLong(ret);
+}
+
+static PyObject *pyelf_elf_ehdr_get(struct pyelf_elf *self)
+{
+	GElf_Ehdr gehdr;
+
+	if ( gelf_getehdr(self->elf, &gehdr) == NULL ) {
+		pyelf_error();
+		return NULL;
+	}
+
+	return pyelf_ehdr_New(&gehdr);
+}
+
+static PyObject *pyelf_elf_ident_get(struct pyelf_elf *self)
+{
+	char *id;
+	size_t sz;
+
+	id = elf_getident(self->elf, &sz);
+	if ( NULL == id ) {
+		pyelf_error();
+		return NULL;
+	}
+
+	return pyelf_bytearray(id, sz);
+}
+
 static PyMethodDef pyelf_elf_methods[] = {
+	{NULL, }
+};
+
+static PyGetSetDef pyelf_elf_attribs[] = {
+	{"kind", (getter)pyelf_elf_kind_get, NULL, "File format"},
+	{"bits", (getter)pyelf_elf_bits_get, NULL, "File format"},
+	{"ehdr", (getter)pyelf_elf_ehdr_get, NULL, "File class"},
+	{"ident", (getter)pyelf_elf_ident_get, NULL, "File class"},
 	{NULL, }
 };
 
@@ -81,6 +197,7 @@ static PyTypeObject elf_pytype = {
 	.tp_init = (initproc)pyelf_elf_init,
 	.tp_dealloc = (destructor)pyelf_elf_dealloc,
 	.tp_methods = pyelf_elf_methods,
+	.tp_getset = pyelf_elf_attribs,
 	.tp_doc = "ELF file",
 };
 
@@ -96,6 +213,8 @@ PyMODINIT_FUNC initelf(void)
 		return;
 
 	if ( PyType_Ready(&elf_pytype) < 0 )
+		return;
+	if ( PyType_Ready(&pyelf_ehdr_pytype) < 0 )
 		return;
 
 	pyelf_err_base = PyErr_NewException("elf.Error",
@@ -115,4 +234,5 @@ PyMODINIT_FUNC initelf(void)
 	PyModule_AddObject(m, "Error", pyelf_err_base);
 	PyModule_AddObject(m, "FormatError", pyelf_err_fmt);
 	PyModule_AddObject(m, "elf", (PyObject *)&elf_pytype);
+	PyModule_AddObject(m, "ehdr", (PyObject *)&pyelf_ehdr_pytype);
 }
